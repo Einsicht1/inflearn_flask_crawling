90a2ea62b61c6d0d170998d5ac7a5425014263bc
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-07-23 22:59:36 +0900

    [Add] crawling

--- inflearn_flask_project2/app.py
+++ inflearn_flask_project2/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
+from selenium import webdriver
+
 app = Flask(__name__)
 write_wb = Workbook()
 write_ws = write_wb.active
@@ -32,12 +34,28 @@ def result():
             write_ws.cell(i,1, daum_list[i-1])
             
         write_wb.save("static/result.xlsx")
-        print(daum_list)
-
-
-
         return render_template("result.html", daum_list = daum_list)
 
+
+@app.route('/naver_shopping')
+def naver_shopping():
+    driver = webdriver.Chrome('/Users/anna/crawling/chromedriver')
+    url = "https://search.shopping.naver.com/search/all?query=%EA%B3%B5%EA%B8%B0%EC%B2%AD%EC%A0%95%EA%B8%B0"
+    driver.get(url)
+    driver.implicitly_wait(3)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    item_html = soup.find_all("li", {"class":"basicList_item__2XT81"})
+    for i in item_html:
+        print(i.find("a", {"class":"basicList_link__1MaTN"}).text)
+
+    driver.find_element_by_css_name("subFilter_filter__3Y-uy").click()
+
+    return render_template('shopping.html')
+    
+
+
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
